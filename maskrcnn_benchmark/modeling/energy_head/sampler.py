5c88ef7c7f42fee874ae39b072c2f205b1e2f081
gulerrberkay <berkay.guler1@gmail.com>
2023-05-21 00:00:11 +0200

    debug

--- maskrcnn_benchmark/modeling/energy_head/sampler.py
+++ maskrcnn_benchmark/modeling/energy_head/sampler.py
@@ -18,7 +18,7 @@ class SGLD(object):
     def normalize_states(self,states):
 
         state_norm = torch.sigmoid(states)
-        bg_score,indices = torch.max(state_norm,dim=1)
+        bg_score,indices = torch.max(state_norm[:,1:],dim=1)
         bg_score = bg_score.reshape(state_norm.shape[0],-1)
         bg_score = 1-bg_score
         #print(values.shape)
