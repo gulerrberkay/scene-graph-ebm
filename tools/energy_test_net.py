960ddc70e81a04108435001d3174a222d3664db3
gulerrberkay <berkay.guler1@gmail.com>
2023-05-23 11:31:09 +0200

    test file change

--- tools/energy_test_net.py
+++ tools/energy_test_net.py
@@ -141,34 +141,34 @@ def main():
     data_loaders_val = make_data_loader(cfg, mode="test", is_distributed=distributed)
 
     for output_folder, dataset_name, data_loader_val in zip(output_folders, dataset_names, data_loaders_val):
-        inference(
-            cfg,
-            base_model,
-            data_loader_val,
-            dataset_name=dataset_name,
-            iou_types=iou_types,
-            box_only=False if cfg.MODEL.RETINANET_ON else cfg.MODEL.RPN_ONLY,
-            device=cfg.MODEL.DEVICE,
-            expected_results=cfg.TEST.EXPECTED_RESULTS,
-            expected_results_sigma_tol=cfg.TEST.EXPECTED_RESULTS_SIGMA_TOL,
-            output_folder=output_folder,
-        )
-        # energy_inference(
+        # inference(
         #     cfg,
         #     base_model,
-        #     energy_model,
-        #     sampler,
         #     data_loader_val,
         #     dataset_name=dataset_name,
-        #     with_sample=False,
         #     iou_types=iou_types,
         #     box_only=False if cfg.MODEL.RETINANET_ON else cfg.MODEL.RPN_ONLY,
         #     device=cfg.MODEL.DEVICE,
         #     expected_results=cfg.TEST.EXPECTED_RESULTS,
         #     expected_results_sigma_tol=cfg.TEST.EXPECTED_RESULTS_SIGMA_TOL,
         #     output_folder=output_folder,
-        #     logger=logger,
         # )
+        energy_inference(
+            cfg,
+            base_model,
+            energy_model,
+            sampler,
+            data_loader_val,
+            dataset_name=dataset_name,
+            with_sample=False,
+            iou_types=iou_types,
+            box_only=False if cfg.MODEL.RETINANET_ON else cfg.MODEL.RPN_ONLY,
+            device=cfg.MODEL.DEVICE,
+            expected_results=cfg.TEST.EXPECTED_RESULTS,
+            expected_results_sigma_tol=cfg.TEST.EXPECTED_RESULTS_SIGMA_TOL,
+            output_folder=output_folder,
+            logger=logger,
+        )
         
         synchronize()
 
