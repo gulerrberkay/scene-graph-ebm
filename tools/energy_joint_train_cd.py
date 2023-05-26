6873d72aa94b3fd7935a5126ab7dcbbad96bdfb7
gulerrberkay <berkay.guler1@gmail.com>
2023-05-26 12:37:44 +0200

    debug test61

--- tools/energy_joint_train_cd.py
+++ tools/energy_joint_train_cd.py
@@ -7,7 +7,7 @@ import os
 import time
 import datetime
 from timeit import default_timer as timer
-os.environ['CUDA_VISIBLE_DEVICES'] = "0,1,2,3,4"
+os.environ['CUDA_VISIBLE_DEVICES'] = "0,1,2,3,4,5"
 
 import torch
 from torch.nn.utils import clip_grad_norm_
