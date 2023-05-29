72ae491c4e740a0b5c9b537ae003f29123ba3063
gulerrberkay <berkay.guler1@gmail.com>
2023-05-29 15:18:57 +0200

    test66-ebm-50det-debug

--- maskrcnn_benchmark/modeling/energy_head/utils.py
+++ maskrcnn_benchmark/modeling/energy_head/utils.py
@@ -147,11 +147,11 @@ def get_predicted_sg(targets,cfg, detections, num_obj_classes, mode, noise_var):
                         flag = 0
                     else:
                         flag = -1
-                    new_new_rel_pair_idxs = new_rel_pair + new_rel_not_pair
+                    new_new_rel_pair_idxs = new_rel_pair #+ new_rel_not_pair
                     new_new_rel_pair_idxs.sort()
                     print(new_rel_pair)
                     print(new_new_rel_pair_idxs)
-
+                    print(filtered_labels)
                     new_new_rel_pair_idxs2 = []
                     #print(deleted_idxs)
                     #print(new_new_rel_pair_idxs)
